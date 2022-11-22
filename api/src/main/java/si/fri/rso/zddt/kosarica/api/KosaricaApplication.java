80cb6db9f55df1ece9ab0e161bd6e27ec754def0
David Trafela <divjad---@users.noreply.github.com>
2022-11-22 11:53:41 +0100

    Added multiple servers

--- api/src/main/java/si/fri/rso/zddt/kosarica/api/KosaricaApplication.java
+++ api/src/main/java/si/fri/rso/zddt/kosarica/api/KosaricaApplication.java
@@ -4,13 +4,19 @@ import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @OpenAPIDefinition(info = @Info(title = "Product catalog API", version = "v1",
         contact = @Contact(email = "dt1193@student.uni-lj.si"),
-        license = @License(name = "dev"), description = "API for managing products."))
+        license = @License(name = "dev"), description = "API for managing products."),
+        servers = {
+                @Server(url = "http://localhost:8080"),
+                @Server(url = "https://www.songify.si/rso")
+        }
+)
 @ApplicationPath("/v1")
 public class KosaricaApplication extends Application {
 }
