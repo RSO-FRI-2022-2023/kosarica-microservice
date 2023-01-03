5a8c4b0b2e656e07e98d137e01b848d6ef29aa26
David Trafela <divjad---@users.noreply.github.com>
2023-01-03 08:23:31 +0100

    Loggiranje

--- api/src/main/java/si/fri/rso/zddt/kosarica/api/KosaricaApplication.java
+++ api/src/main/java/si/fri/rso/zddt/kosarica/api/KosaricaApplication.java
@@ -9,11 +9,11 @@ import org.eclipse.microprofile.openapi.annotations.servers.Server;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@OpenAPIDefinition(info = @Info(title = "Product catalog API", version = "v1",
+@OpenAPIDefinition(info = @Info(title = "Kosarica API", version = "v1",
         contact = @Contact(email = "dt1193@student.uni-lj.si"),
-        license = @License(name = "dev"), description = "API for managing products."),
+        license = @License(name = "dev"), description = "API za pregled in urejanje košarice"),
         servers = {
-                @Server(url = "https://www.songify.si/rso/"),
+                @Server(url = "https://www.songify.si/rso/kosarica/"),
                 @Server(url = "http://localhost:8080/")
         }
 )
